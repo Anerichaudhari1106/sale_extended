2c38d997edee15697d8fc5dc32e881576b62abf1
aneri chaudhari <anerichaudhari116@gmail.com>
2024-11-20 18:15:31 +0530

    [ADD] add report in sale_extended

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Sale_extended',
-    'version': '1.2',
+    'version': '1.3',
     'category': 'sale_extended/sale_extended',
     'summary': 'Sale_extended internal machinery',
 
@@ -13,7 +13,9 @@
     'data': ['security/sale_group.xml',
             'security/ir.model.access.csv',
              'data/ir_cron.xml',
-             'wizard/invoice_wizard_views.xml',
+             'report/ir_actions_report.xml',
+             'report/sale_order_report.xml',
+            'wizard/invoice_wizard_views.xml',
              'data/ir_sequence_data.xml',
              'views/sale_extended_views.xml',
              'views/invoice_views.xml',
